004ff2f498abcca7d64d090fa28cd0157ef8e846
Jeremias Toco <129658828+JeremiasToco@users.noreply.github.com>
2026-03-13 08:29:01 +0100

    Update Http_Request.dart

--- lib/Http/Http_Request.dart
+++ lib/Http/Http_Request.dart
@@ -1,13 +1,8 @@
 class HttpRequest {
-  //----------------------- Estado de produção -----------------------// 
-  static String BASE_URL = "https://api.evistore-ao.com/api/";
-  static String IMG_URL = "https://api.evistore-ao.com/uploads/";
+ 
+  static String BASE_URL = "";
+  static String IMG_URL = "";
 
-  //----------------------- Teste no celular físico -----------------//
-  //static String BASE_URL = "http://192.168.30.46:8000/api/";
-  //static String IMG_URL = "http://192.168.30.46:8000/uploads/";
+  
 
-  //----------------------- Teste na web ---------------------------//
-  //static String BASE_URL = "http://localhost:8000/api/";
-  //static String IMG_URL = "http://localhost:8000/uploads/";
 }
